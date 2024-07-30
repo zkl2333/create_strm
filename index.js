029575043348a3e75f81d0d91a9482371480c623
zkl2333 <i@zkl2333.com>
2024-07-31 00:07:22 +0800

    feat: 🎸 取消总数统计

--- index.js
+++ index.js
@@ -5,28 +5,11 @@ const path = require("path");
 const strmMode = process.env.STRM_MODE || "remote"; // 'local' | 'remote'
 const sourcePath = path.resolve(process.env.SOURCE_PATH || "\\\\Nas001\\共享文件\\影音\\XXX");
 const remoteUrl = process.env.REMOTE_URL || "http://192.168.31.32:8998/媒体库/XXX/";
-const targetPath = path.resolve(process.env.TARGET_PATH || "\\\\Nas001\\共享文件\\影音\\xxxStrm");
+const targetPath = path.resolve(process.env.TARGET_PATH || "\\\\Nas001\\共享文件\\影音\\xxxStrm\\本地上云");
 const overwrite = process.env.OVERWRITE === "true"; // true | false
 
 // 支持的媒体文件扩展名
 const mediaExtensions = [".mp4", ".mkv", ".avi", ".mov", ".flv", ".wmv", ".rmvb", ".webm", ".iso"];
-
-// 计算目录中所有文件的数量
-const countFiles = (dir) => {
-  let fileCount = 0;
-  const walk = (dir) => {
-    fs.readdirSync(dir).forEach((file) => {
-      const filePath = path.join(dir, file);
-      if (fs.statSync(filePath).isDirectory()) {
-        walk(filePath);
-      } else {
-        fileCount++;
-      }
-    });
-  };
-  walk(dir);
-  return fileCount;
-};
 
 // 遍历目录并执行回调函数
 const walk = (dir, callback) => {
@@ -41,7 +24,7 @@ const walk = (dir, callback) => {
 };
 
 // 媒体文件创建 strm 文件，非媒体文件复制
-const createStrm = (dir, totalFiles) => {
+const createStrm = (dir) => {
   let processedFiles = 0;
   walk(dir, (filePath) => {
     const ext = path.extname(filePath);
@@ -85,8 +68,7 @@ const createStrm = (dir, totalFiles) => {
 
     // 更新并显示进度
     processedFiles++;
-    const progress = ((processedFiles / totalFiles) * 100).toFixed(2);
-    console.log(`Progress: ${progress}% (${processedFiles}/${totalFiles} files)`);
+    console.log(`Processed files: ${processedFiles}`);
   });
 };
 
@@ -96,7 +78,4 @@ if (!fs.existsSync(targetPath)) {
   console.log(`Created target directory: ${targetPath}`);
 }
 
-// 计算总文件数并开始创建 strm 文件
-const totalFiles = countFiles(sourcePath);
-console.log(`Total files to process: ${totalFiles}`);
-createStrm(sourcePath, totalFiles);
+createStrm(sourcePath);
